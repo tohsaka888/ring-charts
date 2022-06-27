12ac9700ce18857b157ced1e4ea96e77b41c3b42
tohsaka888 <156132264@qq.com>
2022-06-27 17:10:08 +0800

    feat: 画布

--- src/components/Canvas/index.tsx
+++ src/components/Canvas/index.tsx
@@ -1,15 +1,34 @@
-import React from 'react'
+import React, { useMemo, useState } from 'react'
 import Arc from '../Arc'
+import Legend from '../legend';
+import { CANVASTYPE } from '../type'
 
-function Canvas() {
-  const mx = 250
-  const my = 150
+function Canvas({ width, height, dataSource, radius, title }: CANVASTYPE) {
+  const mx = radius * 2 + 50;
+  const my = height / 2;
+
+  const [activeIndex, setActiveIndex] = useState<number>(-1);
+
+  const total = useMemo(() => {
+    let sum = 0
+    dataSource.forEach((item) => {
+      item.preTotal = sum
+      sum += item.value
+    })
+    return sum
+  }, [dataSource])
+
   return (
-    <svg width="350" height="350">
+    <svg width={width} height={height}>
       {/* <!-- 画圆弧 (rx ry x-axis-rotation large-arc-flag sweep-flag x y) --> */}
-      <Arc r={100} deg={90} width={10} color={'#1890ff'} transform="rotate(60)" mx={mx} my={my} />
-      <Arc r={100} deg={60} width={20} color={'#18ff5d'} mx={mx} my={my} />
-      <Arc r={100} deg={210} width={30} color={'#e29b37'} transform="rotate(150)" mx={mx} my={my} />
+      {dataSource.map((item, index) => {
+        const isActive = index === activeIndex
+        return (
+          <Arc isActive={isActive} key={index} r={radius} deg={item.value / total * 360} width={10 * (index + 1)} color={item.color} transform={`rotate(${item.preTotal as number / total * 360})`} mx={mx} my={my} />
+        )
+      })}
+      <text x={mx - radius} y={my} textAnchor="middle" dominantBaseline={'middle'} fontSize={"1.3rem"} fontWeight={"bold"}>{title}</text>
+      <Legend x={mx + 50} height={height} width={width - mx - 50} dataSource={dataSource} total={total} setActiveIndex={setActiveIndex} />
     </svg>
   )
 }
