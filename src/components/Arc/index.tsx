e9c1f95f600500d85bcca2dc21057cd49528ba55
tohsaka888 <156132264@qq.com>
2022-06-27 15:15:30 +0800

    feat: 测试transform-origin

--- src/components/Arc/index.tsx
+++ src/components/Arc/index.tsx
@@ -1,12 +1,28 @@
-import React from 'react'
-import { ARCTYPE } from '../type';
+import React, { useState } from "react";
+import { ARCTYPE } from "../type";
 
 function Arc({ r, deg, width, color, transform, mx = 0, my = 0 }: ARCTYPE) {
-  const x = r * Math.cos(deg * Math.PI / 180) - r
-  const y = r * Math.sin(deg * Math.PI / 180);
+  const x = r * Math.cos((deg * Math.PI) / 180) - r;
+  const y = r * Math.sin((deg * Math.PI) / 180);
+  const [scaleSize, setScaleSize] = useState<number>(1);
   return (
-    <path d={`M ${mx} ${my} a ${r} ${r} 0 ${deg > 180 ? 1 : 0} 0 ${x} ${y}`} fill="none" stroke={color} strokeWidth={width} transform={transform} />
-  )
+    <g>
+      <path
+        className="arc"
+        onMouseOver={() => {
+          setScaleSize(1.2);
+        }}
+        onMouseOut={() => {
+          setScaleSize(1);
+        }}
+        d={`M ${300} ${300} a ${r} ${r} 0 ${deg > 180 ? 1 : 0} 1 ${x} ${y}`}
+        fill="none"
+        stroke={color}
+        strokeWidth={width * scaleSize}
+        transform={transform}
+      />
+    </g>
+  );
 }
 
-export default Arc
+export default Arc;
