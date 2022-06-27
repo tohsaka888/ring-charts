c2f4c499a4c9095ef8d32e14f84d8039afa71b4f
tohsaka888 <156132264@qq.com>
2022-06-27 17:09:21 +0800

    feat: 组件类型

--- src/components/type.ts
+++ src/components/type.ts
@@ -1,3 +1,5 @@
+import React from "react";
+
 export type ARCTYPE = {
   r: number;
   deg: number;
@@ -6,4 +8,27 @@ export type ARCTYPE = {
   transform?: string;
   mx?: number;
   my?: number;
+  isActive: boolean;
+}
+
+export type DATASOURCE = {
+  name: string;
+  value: number;
+  color: string;
+  preTotal?: number;
+}
+
+
+export type LEGENDTYPE = {
+  dataSource: DATASOURCE[]; //数据源
+  total: number;
+  setActiveIndex: React.Dispatch<React.SetStateAction<number>>;
+};
+
+export type CANVASTYPE = {
+  width: number; // 画布宽度
+  height: number; // 画布高度
+  radius: number; // 圆半径
+  dataSource: DATASOURCE[]; //数据源
+  title?: string;
 }
