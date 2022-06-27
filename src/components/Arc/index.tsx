745c94c00a17ab31336832ba8b3b6313e89f98a4
tohsaka888 <156132264@qq.com>
2022-06-27 17:10:03 +0800

    feat: 圆弧部分

--- src/components/Arc/index.tsx
+++ src/components/Arc/index.tsx
@@ -1,24 +1,24 @@
 import React, { useState } from "react";
 import { ARCTYPE } from "../type";
 
-function Arc({ r, deg, width, color, transform, mx = 0, my = 0 }: ARCTYPE) {
+function Arc({ r, deg, width, color, transform, mx = 0, my = 0, isActive }: ARCTYPE) {
   const x = r * Math.cos((deg * Math.PI) / 180) - r;
   const y = r * Math.sin((deg * Math.PI) / 180);
-  const [scaleSize, setScaleSize] = useState<number>(1);
+  const [scaleSize, setScaleSize] = useState<number>(0);
   return (
     <g>
       <path
         style={{ transformOrigin: `${mx - r}px ${my}px` }}
         onMouseOver={() => {
-          setScaleSize(1.2);
+          setScaleSize(5);
         }}
         onMouseOut={() => {
-          setScaleSize(1);
+          setScaleSize(0);
         }}
         d={`M ${mx} ${my} a ${r} ${r} 0 ${deg > 180 ? 1 : 0} 1 ${x} ${y}`}
         fill="none"
         stroke={color}
-        strokeWidth={width * scaleSize}
+        strokeWidth={isActive ? width + 5 : width + scaleSize}
         transform={transform}
       />
     </g>
