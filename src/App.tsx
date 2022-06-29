856f7154e0fa40fb7f544202ab7877d529202a73
tohsaka888 <156132264@qq.com>
2022-06-29 09:41:16 +0800

    feat: 实例

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,8 @@ import fakeData from './fake_data.json'
 
 function App() {
   return (
-    <div className="App">
-      <Canvas dataSource={fakeData} width={600} height={300} radius={100} title="监控数据" />
+    <div>
+      <Canvas dataSource={fakeData} width={500} height={300} radius={100} title="监控数据" />
     </div>
   );
 }
